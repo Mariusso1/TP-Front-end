9c0470622257aa32c7306a302148d1c6efa5428b
Marius LEGOUALLEC <mlegoual@gmail.com>
2024-03-08 15:24:36 +0100

    s

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import CategorieView from '../views/CategorieView.vue'
+import ClientView from "@/views/ClientView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-    // Associe des chemins d'accès au composant vue à afficher
+  // Associe des chemins d'accès au composant vue à afficher
   routes: [
     {
       path: '/', // Chemin d'accès
@@ -23,6 +24,11 @@ const router = createRouter({
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/clients',
+      name: 'clients',
+      component: ClientView
     }
   ]
 })
